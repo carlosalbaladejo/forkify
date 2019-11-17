3bc1ec10c2bdbcbcb576dbfb266ad685f32de95f
CarlosAlbaladejo <carlos.alba.padilla@gmail.com>
2019-11-17 21:35:50 +0100

    Data handling

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import List from "./models/List";
+import Likes from "./models/Likes";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import * as listView from "./views/listView";
@@ -118,22 +119,6 @@ const controlList = () => {
   });
 };
 
-// Handling recipe button clicks
-elements.recipe.addEventListener("click", e => {
-  if (e.target.matches(".btn-decrease, .btn-decrease *")) {
-    // Decrease button is clicked
-    if (state.recipe.servings > 1) {
-      state.recipe.updateServings("dec");
-      recipeView.updateServingsIngredients(state.recipe);
-    }
-  } else if (e.target.matches(".btn-increase, .btn-increase *")) {
-    state.recipe.updateServings("inc");
-    recipeView.updateServingsIngredients(state.recipe);
-  } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
-    controlList();
-  }
-});
-
 elements.shopping.addEventListener("click", e => {
   const id = e.target.closest(".shopping__item").dataset.itemid;
 
@@ -149,5 +134,57 @@ elements.shopping.addEventListener("click", e => {
     if (val > 0) {
       state.list.updatCount(id, val);
     }
+  }
+});
+
+/**
+ * LIKE CONTROLLER
+ */
+const controlLike = () => {
+  if (!state.likes) {
+    state.likes = new Likes();
+  }
+  const currentID = state.recipe.id;
+
+  if (!state.likes.isLiked(currentID)) {
+    // Add like to the state
+    const newLIke = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img,
+    );
+
+    // Toggle the like button
+
+    // Add like to UI list
+
+  } else {
+    // Remove like from the state
+    state.likes.deleteLike(currentID);
+    // Toggle the like button
+
+    // Remove like from UI list
+
+  }
+};
+
+ // Handling recipe button clicks
+elements.recipe.addEventListener("click", e => {
+  if (e.target.matches(".btn-decrease, .btn-decrease *")) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings("dec");
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches(".btn-increase, .btn-increase *")) {
+    state.recipe.updateServings("inc");
+    recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+    // Add ingredients to shopping list
+    controlList();
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    // Manage liked/unliked recipes
+    controlLike();
   }
 });
--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -11,7 +11,7 @@ export default class Likes {
         return like;
     }
 
-    deleteItem(id) {
+    deleteLike(id) {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
     }
