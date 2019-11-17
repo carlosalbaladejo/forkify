5dc95bb7c757996a6365ebcf67bda123839ec271
CarlosAlbaladejo <carlos.alba.padilla@gmail.com>
2019-11-17 14:49:38 +0100

    Second initial commit

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+// Global app controller
+import Search from "./models/Search";
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from "./views/base";
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2) New search object an add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render the results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+
+// Recipe.js const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
