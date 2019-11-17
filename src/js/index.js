e58cffa0f472668e091500dea11245e4201d7fbf
CarlosAlbaladejo <carlos.alba.padilla@gmail.com>
2019-11-17 22:25:08 +0100

    Prod and final touches

--- src/js/index.js
+++ src/js/index.js
@@ -144,9 +144,6 @@ elements.shopping.addEventListener("click", e => {
 /**
  * LIKE CONTROLLER
  */
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
   if (!state.likes) {
     state.likes = new Likes();
@@ -179,6 +176,21 @@ const controlLike = () => {
   }
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Event handler for page load
+// Restore liekd recipes
+window.addEventListener("load", () => {
+  state.likes = new Likes();
+
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+  // Render existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,26 +1,47 @@
 export default class Likes {
-    constructor() {
-        this.likes = [];
-    }
+  constructor() {
+    this.likes = [];
+  }
 
-    addLike(id, title, author, img) {
-        const like = {
-            id, title, author, img
-        };
-        this.likes.push(like);
-        return like;
-    }
+  addLike(id, title, author, img) {
+    const like = {
+      id,
+      title,
+      author,
+      img
+    };
+    this.likes.push(like);
 
-    deleteLike(id) {
-        const index = this.likes.findIndex(el => el.id === id);
-        this.likes.splice(index, 1);
-    }
+    // Persist the data in local storage
+    this.persistData();
 
-    isLiked(id) {
-        return this.likes.findIndex(el => el.id === id) !== -1;
-    }
+    return like;
+  }
 
-    getNumLikes() {
-        return this.likes.length;
-    }
+  deleteLike(id) {
+    const index = this.likes.findIndex(el => el.id === id);
+    this.likes.splice(index, 1);
+    
+    // Persist the data in local storage
+    this.persistData();
+  }
+
+  isLiked(id) {
+    return this.likes.findIndex(el => el.id === id) !== -1;
+  }
+
+  getNumLikes() {
+    return this.likes.length;
+  }
+
+  persistData() {
+    localStorage.setItem('likes', JSON.stringify(this.likes));
+  }
+
+  readStorage() {
+    const storage = JSON.parse(localStorage.getItem('likes'));
+    
+    // Restore from the local storage
+    if (storage) this.likes = storage;
+  }
 }
